94b0a333401ffc99a891dc52a8b0be61f29ae281
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-12-12 18:39:42 +0900

    call lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,10 @@
 # 将来的には Step Functions や SNS も試したい
 # AWSのLambdaからLambdaを呼んで、Slackにメッセージを送信する - Qiita
 # https://qiita.com/seisyu1985/items/76a7a10543ea38dd07de
-
+import boto3
 from datetime import datetime, timedelta
+import json
+from pprint import pprint
 from pytz import timezone
 from tzlocal import get_localzone
 
@@ -12,9 +14,27 @@ local_zone = get_localzone()
 
 # 名前は Lambda の設定名に合わせる
 def lambda_handler(event, context):
-    utc = datetime.now(timezone("UTC")).strftime(date_format)
-    jst = datetime.now(local_zone).strftime(date_format)
-    print(f'timezone: {local_zone}, UTC: {utc}, JST: {jst}')
+
+    lambda_client = boto3.client("lambda")
+    params = {
+        "symbol": "BTC_JPY",
+        "first": 20001,
+        "last": 20500,
+        "step": 500
+    }
+    response = lambda_client.invoke(
+        FunctionName="bitflyer_data_getter",
+        # 同期実行
+        InvocationType="RequestResponse",
+        Payload=json.dumps(params)
+    )
+
+    res_payload = json.loads(response["Payload"].read())
+    pprint(res_payload)
+
+    # 今回のバッチ実行
+    # 月１の定期実行
+    # ccxt で最新を取得し、その番号と比較して実行判断
 
 
 if __name__ == '__main__':
